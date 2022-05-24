bc4613d490da2918aa62936ef47a4b33931b61b4
Stanwang1210 <ch995308@gmail.com>
2022-05-24 17:25:16 +0800

    update

--- DNN_screen.py
+++ DNN_screen.py
@@ -14,33 +14,39 @@ from time import time
 from capture_win import WindowCapture
 from Detector import face_detect
 
-color = json.load(open('color_table.json', 'r')) # 'RED' 'GREEN' 'BLUE' 'D_GREEN'
+color = json.load(open("color_table.json", "r"))  # 'RED' 'GREEN' 'BLUE' 'D_GREEN'
 window_name = "Faces found"
 modelFile = "models/res10_300x300_ssd_iter_140000.caffemodel"
 configFile = "models/deploy.prototxt"
-eye_cascPath = 'haarcascade_eye.xml'
+eye_cascPath = "haarcascade_eye.xml"
 face_line_width = 2
 eye_line_width = 2
 detect_line_width = 5
 frame_rate = 10
 frame_width, frame_height = 1280, 720
-record_output_name = 'output.mp4'
-out = cv2.VideoWriter(record_output_name, cv2.VideoWriter_fourcc(*'mp4v'), frame_rate, (frame_width, frame_height), isColor=True)
+record_output_name = "output.mp4"
+out = cv2.VideoWriter(
+    record_output_name,
+    cv2.VideoWriter_fourcc(*"mp4v"),
+    frame_rate,
+    (frame_width, frame_height),
+    isColor=True,
+)
 
 eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + eye_cascPath)
 
 cv2.namedWindow(window_name)
-#vc = cv2.VideoCapture(0)
+# vc = cv2.VideoCapture(0)
 net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 
-print('load')
+print("load")
 """
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
 """
-path = 'MLB.com | The Official Site of Major League Baseball - Google Chrome'
+path = "MLB.com | The Official Site of Major League Baseball - Google Chrome"
 
 # initialize the WindowCapture class
 wincap = WindowCapture(path)
@@ -51,9 +57,9 @@ while True:
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     screenshot = wincap.get_screenshot()
     # scale window size
-    scale_window = (1200,675)
+    scale_window = (1200, 675)
     frame = cv2.resize(screenshot, scale_window)
-    
+
     """
     DETECT_FACE = False
     
@@ -65,12 +71,15 @@ while True:
     x2 = int(width * (1-scale))
     #print(x1,y1,x2,y2)
     """
-    
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,(300, 300), (104.0, 117.0, 123.0))
+
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 117.0, 123.0)
+    )
     net.setInput(blob)
     faces = net.forward()
-    eyes = eyeCascade.detectMultiScale(frame, scaleFactor = 1.2, minNeighbors = 4)
-    face_frame = face_detect(frame,faces,eyes,color)
+    eyes = eyeCascade.detectMultiScale(frame, scaleFactor=1.2, minNeighbors=4)
+    face_frame, face_count = face_detect(frame, faces, eyes, color)
+    print(f"{face_count} faces detected !!")
     """
     # for (x, y, w, h) in faces:
     #     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
@@ -102,17 +111,16 @@ while True:
     face_frame = cv2.cvtColor(face_frame, cv2.COLOR_BGR2RGB)
     face_frame = cv2.resize(face_frame, (frame_width, frame_height))
     out.write(face_frame)
-    cv2.imshow(window_name,face_frame)
-    
+    cv2.imshow(window_name, face_frame)
 
     key = cv2.waitKey(1)
 
     # debug the loop rate
-    print('FPS {}'.format(1 / (time() - loop_time)))
+    print("FPS {}".format(1 / (time() - loop_time)))
     loop_time = time()
-    #rval, frame = vc.read()
+    # rval, frame = vc.read()
 
-    if key == 27: # exit on ESC
+    if key == 27:  # exit on ESC
         break
 
 # vc.release()
